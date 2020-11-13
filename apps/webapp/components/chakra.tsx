15567a08fd9dbb9eb24e1a2ee63381e7b6d9d07f
k4m4 <nikolaskam@gmail.com>
2020-11-13 20:49:48 +0200

    Implement Chakra ColorModeScript

---
+++ apps/webapp/components/chakra.tsx
@@ -0,0 +1,27 @@
+import {
+  ChakraProvider,
+  cookieStorageManager,
+  localStorageManager,
+} from '@chakra-ui/core'
+
+const Chakra = ({ cookies, children }) => {
+    const colorModeManager = typeof cookies === 'string'
+        ? cookieStorageManager(cookies)
+        : localStorageManager
+
+    return (
+        <ChakraProvider colorModeManager={colorModeManager}>
+            {children}
+        </ChakraProvider>
+    )
+}
+
+export default Chakra
+
+export const getServerSideProps = ({ req: request }) => {
+    return {
+        props: {
+            cookies: request.headers.cookie ?? '',
+        },
+    }
+}
