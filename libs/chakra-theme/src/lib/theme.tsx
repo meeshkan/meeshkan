fba0ed95a647e016f439fe87bb7a36419726c053
k4m4 <nikolaskam@gmail.com>
2020-11-13 20:39:14 +0200

    Comment out unused theme logic

--- libs/chakra-theme/src/lib/theme.tsx
+++ libs/chakra-theme/src/lib/theme.tsx
@@ -4,14 +4,14 @@ import globalStyles from "./global"
 import customComponents from "./components"
 import { mode, getColor, transparentize } from "@chakra-ui/theme-tools"
 
-type Dict = Record<string, any>
+// type Dict = Record<string, any>
 
-function getBg(props: Dict) {
-	const { theme, colorScheme: c } = props
-	const lightBg = getColor(theme, `${c}.100`, c)
-	const darkBg = transparentize(`${c}.200`, 0.16)(theme)
-	return mode(lightBg, darkBg)(props)
-}
+// function getBg(props: Dict) {
+// 	const { theme, colorScheme: c } = props
+// 	const lightBg = getColor(theme, `${c}.100`, c)
+// 	const darkBg = transparentize(`${c}.200`, 0.16)(theme)
+// 	return mode(lightBg, darkBg)(props)
+// }
 
 const customTheme = extendTheme({
 	config: {
