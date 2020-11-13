dfeaaddcd60c25b6d64747d142b8786f80c9d82c
k4m4 <nikolaskam@gmail.com>
2020-11-13 21:03:00 +0200

    Create Layout component

---
+++ apps/webapp/components/layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Flex, useColorModeValue } from '@chakra-ui/core'
+
+const Layout = ({ children, ...props }) => {
+    return (
+        <Flex
+            p={[0, 8, 8, 8]}
+            w="100%"
+            bg={useColorModeValue('gray.100', 'gray.900')}
+            h={['100%', '100vh', '100vh', '100vh']}
+            direction={['column', 'row', 'row', 'row']}
+            {...props}
+        >
+            {children}
+        </Flex>
+    )
+}
+
+export default Layout
