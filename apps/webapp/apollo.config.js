dcb3c7a33796b6033ad5616bb497083900e4e907
Makenna Smutz <makennasmutz@gmail.com>
2020-11-16 15:57:57 +0100

    apollo server config and local env

---
+++ apps/webapp/apollo.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	client: {
+		service: 'Meeshkan-deoux',
+		url: 'http://localhost:4200/api/graphql',
+	},
+};
--- apps/webapp/pages/api/graphql.tsx
+++ apps/webapp/pages/api/graphql.tsx
@@ -2,22 +2,26 @@ import { ApolloServer } from 'apollo-server-micro';
 import { schema } from '../../utils/graphql/schema';
 
 const apolloServer = new ApolloServer({
-  schema,
-  introspection: true,
-  playground: true,
-  context(ctx) {
-    return ctx;
-  },
+	schema,
+	introspection: true,
+	playground: true,
+	// mocks: true,
+	apollo: {
+		key: 'service:Meeshkan-deoux:mdh4KcLcbbdrR3ZXPExkjg',
+	},
+	context(ctx) {
+		return ctx;
+	},
 });
 
 export const config = {
-  api: {
-    bodyParser: false,
-  },
-  engine: {
-    reportSchema: true,
-    variant: 'current',
-  },
+	api: {
+		bodyParser: false,
+	},
+	engine: {
+		reportSchema: true,
+		variant: 'current',
+	},
 };
 
 export default apolloServer.createHandler({ path: '/api/graphql' });
