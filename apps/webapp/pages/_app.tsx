cb7f0360c18e651bc7d8a4dbe23505a8ca981b16
k4m4 <nikolaskam@gmail.com>
2020-11-17 13:31:27 +0200

    Remove redundant imports from pages/_app

--- apps/webapp/pages/_app.tsx
+++ apps/webapp/pages/_app.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
-import { ChakraProvider } from '@chakra-ui/core'
-import customTheme from '@frontend/chakra-theme'
 
 const CustomApp = ({ Component, pageProps }: AppProps) => {
   return (
