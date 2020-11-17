9b2328fb12a4f28a7cc88b7dcab939105046295e
k4m4 <nikolaskam@gmail.com>
2020-11-17 12:13:47 +0200

    Create Grid container

---
+++ apps/webapp/components/grid.tsx
@@ -0,0 +1,16 @@
+import { Box, useColorModeValue } from '@chakra-ui/core'
+
+const Grid = (props) => {
+    return (
+        <Box
+            p={8}
+            bg={useColorModeValue('gray.100', 'gray.800')}
+            w="100%"
+            h="100%"
+            rounded="md"
+            {...props}
+        />
+    )
+}
+
+export default Grid
