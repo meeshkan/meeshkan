65cb50d761204aa3d25489c33da73406df91a5e2
k4m4 <nikolaskam@gmail.com>
2020-11-17 12:16:29 +0200

    Turn Layout container to stack

--- apps/webapp/components/layout.tsx
+++ apps/webapp/components/layout.tsx
@@ -1,18 +1,20 @@
-import { Flex, useColorModeValue } from '@chakra-ui/core'
+import { Stack, useColorModeValue } from '@chakra-ui/core'
 import './layout.module.css'
 
 const Layout = ({ children, ...props }) => {
     return (
-        <Flex
+        <Stack
             p={[0, 8, 8, 8]}
             bg={useColorModeValue('gray.200', 'gray.900')}
             w="100%"
             h="100vh"
+            d="flex"
             direction={['column', 'row', 'row', 'row']}
+            spacing={[4, 8, 8, 8]}
             {...props}
         >
             {children}
-        </Flex>
+        </Stack>
     )
 }
 
