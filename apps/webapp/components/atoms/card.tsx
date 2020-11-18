30880dd16b18b08ab764403b38fec796deaf0b62
k4m4 <nikolaskam@gmail.com>
2020-11-18 15:40:43 +0200

    Create Card atom

---
+++ apps/webapp/components/atoms/card.tsx
@@ -0,0 +1,14 @@
+import { Box, useColorModeValue } from '@chakra-ui/react'
+
+const Card = (props) => {
+    return (
+        <Box
+            p={4}
+            rounded="lg"
+			bg={useColorModeValue('gray.100', 'gray.800')}
+            {...props}
+        />
+    )
+}
+
+export default Card
