58b620f52cedfba2ffe67410989032c42f96f132
k4m4 <nikolaskam@gmail.com>
2020-11-25 22:19:06 +0200

    Remove redundant utils/graphql & ApolloProvider

--- apps/webapp/pages/index.tsx
+++ apps/webapp/pages/index.tsx
@@ -1,5 +1,3 @@
-import { ApolloProvider } from '@apollo/react-hooks';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
 import ChakraProvider from '../components/molecules/chakra';
 import Layout from '../components/templates/layout';
 import SideBar from '../components/organisms/sidebar';
@@ -11,20 +9,13 @@ type IndexProps = {
 };
 
 const Index = ({ cookies }: IndexProps) => {
-	const client = new ApolloClient({
-		uri: 'http://localhost:4200/api/graphql',
-		cache: new InMemoryCache(),
-	});
-
 	return (
-		<ApolloProvider client={client}>
-			<ChakraProvider cookies={cookies}>
-				<Layout>
-					<SideBar />
-					<Grid />
-				</Layout>
-			</ChakraProvider>
-		</ApolloProvider>
+		<ChakraProvider cookies={cookies}>
+			<Layout>
+				<SideBar />
+				<Grid />
+			</Layout>
+		</ChakraProvider>
 	);
 };
 
