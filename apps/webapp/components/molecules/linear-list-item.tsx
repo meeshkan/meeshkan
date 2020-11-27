fcb0bde51dd71fa60170ec88ec0a6ba876188c46
Makenna Smutz <makennasmutz@gmail.com>
2020-11-27 15:36:36 +0100

    linear button

--- apps/webapp/components/molecules/linear-list-item.tsx
+++ apps/webapp/components/molecules/linear-list-item.tsx
@@ -1,11 +1,13 @@
 import {
 	ListItem,
 	Flex,
+	Button,
 	Link,
 	Avatar,
 	useColorModeValue,
 } from '@chakra-ui/react';
 import truncate from 'truncate';
+import { transparentize } from '@chakra-ui/theme-tools';
 
 type LinearListItemProps = {
 	title: string;
@@ -15,12 +17,31 @@ type LinearListItemProps = {
 
 const LinearListItem = ({ title, author, avatar }: LinearListItemProps) => {
 	return (
-		<ListItem as={Flex} align="center" justify="space-between">
-			<Link color={useColorModeValue('gray.900', 'gray.200')}>
-				{truncate(title, 30)}
-			</Link>
+		<Button
+			as={Link}
+			variant="ghost"
+			size="sm"
+			colorScheme="gray"
+			_active={{
+				backgroundColor: useColorModeValue('gray.100', 'gray.800'),
+				color: useColorModeValue('gray.900', 'white'),
+			}}
+			_hover={{
+				// @ts-ignore
+				backgroundColor: useColorModeValue(
+					transparentize('gray.100', 0.75),
+					transparentize('gray.800', 0.75)
+				),
+				textDecor: 'none',
+			}}
+			w="full"
+			d="flex"
+			alignItems="center"
+			justifyContent="space-between"
+		>
+			<ListItem>{truncate(title, 30)}</ListItem>
 			<Avatar size="xs" name={author} src={avatar} ml={4} />
-		</ListItem>
+		</Button>
 	);
 };
 
