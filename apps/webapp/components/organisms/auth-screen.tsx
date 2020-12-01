a0c9ae51e3baa6551cce3e88a764fc0b7cb13a39
Makenna Smutz <makennasmutz@gmail.com>
2020-12-01 10:23:59 +0100

    add favicon and change sign in button

--- apps/webapp/components/organisms/auth-screen.tsx
+++ apps/webapp/components/organisms/auth-screen.tsx
@@ -1,14 +1,16 @@
 import { LogoIcon } from '@frontend/chakra-theme';
-import { Flex, Button } from '@chakra-ui/react';
+import { Flex, Button, LightMode } from '@chakra-ui/react';
 import { goToLogin } from '../../utils/user';
 
 const AuthScreen = () => {
 	return (
 		<Flex align="center" justify="center" direction="column" h="100vh">
 			<LogoIcon width="auto" height={10} />
-			<Button colorScheme="red" mt={5} onClick={() => goToLogin()}>
-				Sign in / Sign up
-			</Button>
+			<LightMode>
+				<Button colorScheme="blue" mt={5} onClick={() => goToLogin()}>
+					Sign in / Sign up
+				</Button>
+			</LightMode>
 		</Flex>
 	);
 };
