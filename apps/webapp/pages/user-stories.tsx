f51d0af98ad9f80b45a76753e15c8fa229fa7182
Makenna Smutz <makennasmutz@gmail.com>
2020-12-01 18:14:29 +0100

    styling of getting started card

--- apps/webapp/pages/user-stories.tsx
+++ apps/webapp/pages/user-stories.tsx
@@ -1,10 +1,78 @@
 import Layout from '../components/templates/layout';
-import { Box } from '@chakra-ui/react';
+import { Box, Stack, Text, useColorModeValue } from '@chakra-ui/react';
+import GridCard from '../components/molecules/grid-card';
+import { BookIcon, ChatIcon, PlusIcon } from '@frontend/chakra-theme';
+import { transparentize } from '@chakra-ui/theme-tools';
+
+function StartButton({ icon, text }) {
+	return (
+		<Box
+			d="flex"
+			alignItems="center"
+			// @ts-ignore
+			backgroundColor={useColorModeValue(
+				'cyan.50',
+				transparentize('cyan.500', 0.15)
+			)}
+			color={useColorModeValue('cyan.700', 'cyan.50')}
+			fontWeight={600}
+			p={4}
+			w="100%"
+			borderRadius="md"
+		>
+			{icon}
+			<Text ml={4}>{text}</Text>
+		</Box>
+	);
+}
 
 const UserStoriesPage = () => {
 	return (
 		<Layout>
-			<Box>Hello world</Box>
+			<Stack p={[6, 0, 0, 0]} w="100%" rounded="lg" spacing={6}>
+				<GridCard title="Getting started">
+					<Stack direction="row" spacing="32px">
+						<StartButton
+							icon={
+								<BookIcon
+									boxSize={8}
+									color={useColorModeValue('cyan.500', 'cyan.300')}
+									strokeWidth="2.5"
+								/>
+							}
+							text="Read the documentation"
+						/>
+						<StartButton
+							icon={
+								<ChatIcon
+									boxSize={8}
+									color={useColorModeValue('cyan.500', 'cyan.300')}
+									strokeWidth="2.5"
+								/>
+							}
+							text="Chat with an expert"
+						/>
+						<Box
+							d="flex"
+							alignItems="center"
+							border="1px dashed"
+							// @ts-ignore
+							borderColor={useColorModeValue('gray.500', 'gray.300')}
+							fontWeight={600}
+							p={4}
+							w="100%"
+							borderRadius="md"
+						>
+							<PlusIcon
+								boxSize={8}
+								color={useColorModeValue('gray.500', 'gray.300')}
+								strokeWidth="2.5"
+							/>
+							<Text ml={4}>Create a new user story</Text>
+						</Box>
+					</Stack>
+				</GridCard>
+			</Stack>
 		</Layout>
 	);
 };
