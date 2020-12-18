62e3441eea8f8953f51d4f953f4085748d75cc7f
k4m4 <nikolaskam@gmail.com>
2020-12-18 11:00:38 +0200

    Close nav on page navigation

--- apps/webapp/components/organisms/sidebar.tsx
+++ apps/webapp/components/organisms/sidebar.tsx
@@ -1,13 +1,22 @@
+import { useEffect } from 'react';
 import { Flex, useColorModeValue, useMediaQuery } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 import { useCycle } from 'framer-motion';
 import MotionStack from '../atoms/motion-stack';
 import SideBarHeader from '../molecules/sidebar-header';
 import SideBarBody from '../molecules/sidebar-body';
 
 const SideBar = (props) => {
+	const { pathname } = useRouter();
 	const [isOpen, toggleOpen] = useCycle(false, true);
 	const [isSmallScreen] = useMediaQuery('(max-width: 62em)');
 	const stackSmallHeight = isOpen ? '100%' : 'auto';
+
+	useEffect(() => {
+		if (isOpen) {
+			toggleOpen();
+		}
+	}, [pathname]);
 
 	return (
 		<MotionStack
