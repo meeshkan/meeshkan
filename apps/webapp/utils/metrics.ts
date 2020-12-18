e638600c3c4d3c9dc8477290e8f1b25a6727492d
Mike Solomon <mike@meeshkan.com>
2020-12-18 13:50:58 +0200

    Confidence score

--- apps/webapp/utils/metrics.ts
+++ apps/webapp/utils/metrics.ts
@@ -122,13 +122,15 @@ export const getLastSevenDaysInFormat = (format: string) =>
 	lastSevenDays.map((day) => day.format(format));
 
 export const getConfidenceScore = (
-	userStories: UserStories
+	userStories: UserStories['items']
 ): DisplayableMetric => {
 	const confidenceFactors: ConfidenceFactors = {
 		mainBranch: {
 			name: 'main',
-			totalRecordings: userStories.count,
-			testCases: userStories?.items?.filter((story) => story.isTestCase)
+			totalRecordings: userStories.length,
+			totalIsTestCase: userStories.filter((story) => story.isTestCase).length,
+			testRuns: userStories
+				.filter((story) => story.isTestCase)
 				.map((story) =>
 					story.testRuns.items
 						.filter(
@@ -156,6 +158,6 @@ export const getConfidenceScore = (
 	return {
 		value: createConfidenceScore(confidenceFactors) * 100,
 		percentageChange: 0.0, // TODO: un-hard-code when we have a meaningful sense of window
-		dataPoints: userStories.count,
+		dataPoints: userStories.length,
 	};
 };
