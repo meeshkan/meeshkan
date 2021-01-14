6019dfd935702cb8e7bbc91b0dd3610c48f55a5b
k4m4 <nikolaskam@gmail.com>
2021-01-14 15:15:39 +0200

    Add doughnut to "Latest case status" card

--- apps/webapp/pages/[projectName]/test-runs/index.tsx
+++ apps/webapp/pages/[projectName]/test-runs/index.tsx
@@ -1,10 +1,44 @@
+import { Stack, Text, useColorModeValue } from '@chakra-ui/react';
+import { Bar, Doughnut } from 'react-chartjs-2';
+import theme from '@frontend/chakra-theme';
+import GridCard from '../../../components/molecules/grid-card';
 import Card from '../../../components/atoms/card';
 import { useValidateSelectedProject } from '../../../hooks/use-validate-selected-project';
 import LoadingScreen from '../../../components/organisms/loading-screen';
 import NotFoundError from '../../404';
 
+const doughnutDataValues = [80, 8, 12];
+const doughnutData = {
+	labels: ['Passing', 'Didn\'t run', 'Failure'],
+	datasets: [
+		{
+			data: doughnutDataValues, 
+			backgroundColor: [
+				theme.colors.blue[500],
+				theme.colors.blue[700],
+				theme.colors.blue[900],
+			],
+			borderColor: theme.colors.transparent,
+		},
+	],
+};
+
 const TestRunsPage = () => {
-    const { found, loading } = useValidateSelectedProject();
+	const { found, loading } = useValidateSelectedProject();
+
+	const doughnutOptions = {
+		legend: {
+			labels: {
+				boxWidth: 12,
+				fontColor: useColorModeValue(
+					theme.colors.gray[600],
+					theme.colors.gray[400]
+				),
+			},
+			position: 'right',
+			align: 'center',
+		},
+	};
 
 	if (loading) {
 		return <LoadingScreen as={Card} />;
@@ -14,7 +48,19 @@ const TestRunsPage = () => {
 		return <NotFoundError />;
 	}
 
-    return null;
+	return (
+		<Stack p={[6, 0, 0, 0]} w="100%" spacing={6}>
+			<GridCard title="Latest case status">
+				{doughnutDataValues.length > 0 ? (
+					<Doughnut data={doughnutData} options={doughnutOptions} />
+				) : (
+					<Text fontStyle="italic">
+						No tests have been run yet.
+					</Text>
+				)}
+			</GridCard>
+		</Stack>
+	);
 };
 
 export default TestRunsPage;
