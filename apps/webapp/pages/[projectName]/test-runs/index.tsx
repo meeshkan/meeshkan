5fab2a39ada2002110dc40587d66cb84e30dc786
k4m4 <nikolaskam@gmail.com>
2021-01-20 11:47:35 +0200

    Customize doughnut legend

--- apps/webapp/pages/[projectName]/test-runs/index.tsx
+++ apps/webapp/pages/[projectName]/test-runs/index.tsx
@@ -3,6 +3,10 @@ import {
 	Flex,
 	Stack,
 	Text,
+	Box,
+	List,
+	ListItem,
+	Center,
 	useColorModeValue,
 } from '@chakra-ui/react';
 import { Doughnut } from 'react-chartjs-2';
@@ -16,17 +20,21 @@ import LoadingScreen from '../../../components/organisms/loading-screen';
 import NotFoundError from '../../404';
 import { UserContext } from '../../../utils/user';
 
+const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
+
 const doughnutDefaultDataValues = [80, 8, 12];
+const doughnutBackgroundColors = [
+	theme.colors.blue[500],
+	theme.colors.blue[700],
+	theme.colors.blue[900],
+];
+
 const doughnutData = {
-	labels: ['Passing', "Didn't run", 'Failure'],
+	labels: ['Passing', 'Didn\'t run', 'Failure'],
 	datasets: [
 		{
 			data: doughnutDefaultDataValues,
-			backgroundColor: [
-				theme.colors.blue[500],
-				theme.colors.blue[700],
-				theme.colors.blue[900],
-			],
+			backgroundColor: doughnutBackgroundColors,
 			borderColor: theme.colors.transparent,
 		},
 	],
@@ -42,31 +50,28 @@ const TestRunsPage = () => {
 	)[0];
 
 	const latestTestRunStats = latestTestRun && _.countBy(
-		latestTestRun.testOutcome.items.map(outcome => outcome.status)
+		latestTestRun.testOutcome.items
+			.map(outcome => capitalize(outcome.status))
 			.reduce((pre, cur) => pre.concat(cur), []) || []
 	);
 
-	const validTestRunStatus = ['passing', 'failing', 'did not run'];
+	const validTestRunStatus = ['Passing', 'Failing', 'Did not run'];
 	Object.keys(latestTestRunStats || {}).forEach(
 		(key) => validTestRunStatus.includes(key) || delete latestTestRunStats[key]
 	);
 
 	const doughnutDataValues = Object.values(latestTestRunStats || {});
+	const totalTestRunOutcomes = doughnutDataValues.reduce((a, b) => a + b, 0);
 	doughnutData.datasets[0].data = doughnutDataValues;
-	doughnutData.labels = Object.keys(latestTestRunStats || {});
+	const doughnutDataLabels = Object.keys(latestTestRunStats || {});
+	doughnutData.labels = doughnutDataLabels;
 
 	const doughnutOptions = {
 		legend: {
-			labels: {
-				boxWidth: 12,
-				fontColor: useColorModeValue(
-					theme.colors.gray[600],
-					theme.colors.gray[400]
-				),
-			},
-			position: 'right',
-			align: 'center',
+			display: false,
 		},
+		responsive: false,
+    	maintainAspectRatio: false,
 	};
 
 	if (loading) {
@@ -79,31 +84,64 @@ const TestRunsPage = () => {
 
 	return (
 		<Flex direction="column" w="100%" p={[6, 0, 0, 0]}>
-			<GridCard title="Latest case status" mb={6} flex="0 0 auto">
-				{doughnutDataValues.length > 0 ? (
-					<Doughnut data={doughnutData} options={doughnutOptions} height={30} />
-				) : (
-					<Text fontStyle="italic">We don't have enough test runs just yet.</Text>
-				)}
-			</GridCard>
-			<Stack spacing={6} overflowY="scroll">
-				{testRuns
-					.map((testRun, index) => {
-						const { status, createdAt } = testRun;
-						return (
-							<TestRunCard
-								status={status}
-								runNumber={index + 1}
-								date={new Date(createdAt)}
-								stats={_.countBy(
-									testRun.testOutcome.items
-										.map(outcome => outcome.status)
-								)}
-							/>
-						);
-					})
-				}
-			</Stack>
+			{doughnutDataValues.length > 0 && (
+				<GridCard title="Latest case status" mb={6} flex="0 0 auto">
+					<Flex justify="center" align="center" direction={['column', 'column', 'row', 'row']}>
+						<Doughnut
+							data={doughnutData}
+							options={doughnutOptions}
+							height={150}
+							width={125}
+						/>
+						<List d="flex" ml={2}>
+							{doughnutDataLabels.map((label, index) => {
+								return (
+									<ListItem key={label} mx={5} d="flex" flexDirection="column" alignItems="center">
+										<Text fontSize="40px" fontWeight={700}>
+											{Math.round(latestTestRunStats[label] / totalTestRunOutcomes * 100)}%
+										</Text>
+										<Flex align="center">
+											<Box
+												bg={doughnutBackgroundColors[index]}
+												border="1px solid rgba(0, 0, 0, .2)"
+												w={4}
+												h={4}
+											/>
+											<Text ml={3}>{label}</Text>
+										</Flex>
+									</ListItem>
+								);
+							})}
+						</List>
+					</Flex>
+				</GridCard>
+			)}
+			{testRuns.length > 0 ? (
+				<Stack spacing={6} overflowY="scroll">
+					{testRuns
+						.map((testRun, index) => {
+							const { status, createdAt } = testRun;
+							return (
+								<TestRunCard
+									status={status}
+									runNumber={index + 1}
+									date={new Date(createdAt)}
+									stats={_.countBy(
+										testRun.testOutcome.items
+											.map(outcome => outcome.status)
+									)}
+								/>
+							);
+						})
+					}
+				</Stack>
+			) : (
+				<Center h="100%" as={Card}>
+					<Text fontStyle="italic" fontSize="lg">
+						There are no test runs for this project just yet.
+					</Text>
+				</Center>
+			)}
 		</Flex>
 	);
 };
