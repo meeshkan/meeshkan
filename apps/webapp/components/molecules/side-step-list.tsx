1c3d4f749438c21ae87db23ded5d086652c63943
Makenna Smutz <makenna@meeshkan.com>
2021-02-01 10:52:46 +0100

    remove xpath in steps (#67)

--- apps/webapp/components/molecules/side-step-list.tsx
+++ apps/webapp/components/molecules/side-step-list.tsx
@@ -5,7 +5,6 @@ export const StepList = ({ steps }) =>
 	steps.map((step, index) => {
 		const steps = [];
 		typeof step.target == 'string' ? steps.push(step.target) : null;
-		typeof step.target == 'object' ? steps.push(step.target.xpath) : null;
 		typeof step.target == 'object' ? steps.push(step.target.selector) : null;
 
 		return (
