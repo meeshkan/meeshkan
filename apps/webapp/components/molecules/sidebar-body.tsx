82c9fb249bf90b1f25a39771868663a69a6c6b82
Makenna Smutz <makennasmutz@gmail.com>
2021-02-12 10:31:56 +0100

    make adjustments to project picker and add it to settings

--- apps/webapp/components/molecules/sidebar-body.tsx
+++ apps/webapp/components/molecules/sidebar-body.tsx
@@ -78,7 +78,7 @@ const SideBarBody = () => {
 						</Heading>
 					</a>
 				</Link>
-				<Stack mt={6} spacing={6}>
+				<Stack mt={6} spacing={6} h="100%">
 					<Box>
 						<Flex align="flex-start">
 							<Box
@@ -204,6 +204,107 @@ const SideBarBody = () => {
 									Integrations
 								</NavButton>
 							</Stack>
+						</Flex>
+					</Box>
+
+					<Spacer />
+
+					<Box>
+						<NavButton onClick={showIntercom} leftIcon={<ChatIcon />} mt={2}>
+							Help and Feedback
+						</NavButton>
+						<Divider my={4} />
+						<Flex align="center">
+							<Menu>
+								<MenuButton
+									as={Button}
+									p={0}
+									m={0}
+									size="sm"
+									colorScheme="gray"
+									// @ts-expect-error
+									backgroundColor={useColorModeValue(
+										'gray.50',
+										transparentize('gray.800', 0.75)
+									)}
+									rightIcon={<ArrowUpDownIcon mr={3} />}
+									w="100%"
+									textAlign="left"
+								>
+									<Flex
+										align="center"
+										color={useColorModeValue('gray.500', 'gray.300')}
+										fontWeight="600"
+										maxW="20ch"
+										whiteSpace="nowrap"
+										overflow="hidden"
+									>
+										<Avatar
+											src={avatarUrl}
+											name={project?.name}
+											icon={
+												<QuestionIcon
+													color={useColorModeValue('gray.400', 'white')}
+													fontSize="1rem"
+												/>
+											}
+											color={useColorModeValue('gray.700', 'gray.200')}
+											bg={useColorModeValue('gray.200', 'gray.600')}
+											size="sm"
+											borderRadius="md"
+											mr={3}
+										/>
+										{project?.name}
+									</Flex>
+								</MenuButton>
+								<MenuList>
+									<MenuOptionGroup
+										defaultValue={project?.name}
+										title="Projects"
+										type="radio"
+									>
+										{projects.map((project) => (
+											<MenuItemOption
+												key={project.id}
+												value={project.name}
+												onClick={() => setProject(project)}
+											>
+												<Flex display="flex" alignItems="center">
+													<Avatar
+														src={project.avatar?.downloadUrl}
+														name={project.name}
+														icon={
+															<QuestionIcon
+																color={useColorModeValue('gray.400', 'white')}
+																fontSize="1rem"
+															/>
+														}
+														color={useColorModeValue('gray.700', 'gray.200')}
+														bg={useColorModeValue('gray.200', 'gray.600')}
+														size="xs"
+														borderRadius="md"
+														mr={3}
+													/>
+													<Text fontSize="sm" fontWeight="600">
+														{project.name}
+													</Text>
+												</Flex>
+											</MenuItemOption>
+										))}
+									</MenuOptionGroup>
+								</MenuList>
+							</Menu>
+							<IconButton
+								isDisabled
+								aria-label="Settings"
+								colorScheme="gray"
+								color={useColorModeValue('gray.500', 'gray.400')}
+								icon={<SettingsIcon />}
+								onClick={() => router.push(`/${slugifiedProjectName}/settings`)}
+								variant="ghost"
+								size="sm"
+								ml={2}
+							/>
 						</Flex>
 					</Box>
 				</Stack>
