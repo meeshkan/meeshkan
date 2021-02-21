66dd2baa3725fd87001ab62c59d62d98969ce467
Makenna Smutz <makennasmutz@gmail.com>
2021-02-21 20:38:48 +0100

    temporarily remove the aspect ratio box

--- apps/webapp/components/atoms/video-player.tsx
+++ apps/webapp/components/atoms/video-player.tsx
@@ -2,18 +2,19 @@ import React from 'react';
 import { AspectRatio, useColorModeValue } from '@chakra-ui/react';
 
 const VideoPlayer = ({ children }) => (
-	<AspectRatio
-		ratio={16 / 9}
-		borderRadius="md"
-		overflow="hidden"
-		border="1px solid"
-		borderColor={useColorModeValue('gray.200', 'gray.700')}
-	>
-		<video controls>
-			{children}
-			Your browser does not support the video tag.
-		</video>
-	</AspectRatio>
+	// <AspectRatio
+	// 	ratio={16 / 9}
+	// 	borderRadius="md"
+	// 	overflow="hidden"
+	// 	border="1px solid"
+	// 	borderColor={useColorModeValue('gray.200', 'gray.700')}
+	// 	backgroundColor="red.500"
+	// >
+	<video style={{ borderRadius: '6px' }} controls>
+		{children}
+		Your browser does not support the video tag.
+	</video>
+	// </AspectRatio>
 );
 
 export default VideoPlayer;
