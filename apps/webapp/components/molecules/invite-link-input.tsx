320d38b15613b8e9b3e4fcb1d41c61325801577e
Makenna Smutz <makennasmutz@gmail.com>
2021-02-21 19:43:38 +0100

    Add the button and toast for triggering the refresh button

--- apps/webapp/components/molecules/invite-link-input.tsx
+++ apps/webapp/components/molecules/invite-link-input.tsx
@@ -1,16 +1,25 @@
-import { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import {
 	InputGroup,
 	Input,
 	InputRightElement,
 	IconButton,
+	Button,
+	Flex,
+	useColorModeValue,
+	useToast,
 } from '@chakra-ui/react';
 import { CopyIcon } from '@frontend/chakra-theme';
 import { UserContext } from '../../utils/user';
 import { useClipboard } from '../../hooks/use-clipboard';
+import { eightBaseClient } from '../../utils/graphql';
+import { REFRESH_INVITE_LINK } from '../../graphql/project';
 
 const InviteLinkInput = () => {
-	const { project } = useContext(UserContext);
+	const { project, idToken } = useContext(UserContext);
+	const [inviteLink, setInviteLink] = useState(
+		project.configuration.inviteLink
+	);
 	const { onCopy } = useClipboard({
 		toastTitle: `This project's script is copied to clipboard.`,
 		toastMessage: 'Paste it within the `head` of your app.',
@@ -18,24 +27,60 @@ const InviteLinkInput = () => {
 		status: 'info',
 	});
 
+	const toast = useToast();
+	const client = eightBaseClient(idToken);
+
+	const refreshInviteLink = async () => {
+		const request = await client
+			.request(REFRESH_INVITE_LINK, {
+				projectID: project.id,
+			})
+			.then((res) => {
+				toast({
+					position: 'bottom-right',
+					title: 'Successfully refreshed invite link.',
+					description:
+						"Copy the invite link and send to anyone you'd like to add to your project.",
+					isClosable: true,
+					status: 'success',
+					variant: 'clean',
+				});
+				setInviteLink(res.projectUpdate.configuration.inviteLink);
+			});
+	};
+
 	return (
-		<InputGroup mb={4}>
-			<Input
-				value={project.configuration.inviteLink}
-				color="blue.400"
-				onClick={onCopy}
-				isReadOnly
-			/>
-			<InputRightElement>
-				<IconButton
-					icon={<CopyIcon color="gray.500" />}
-					aria-label="Copy invite link"
+		<Flex>
+			<InputGroup mb={4}>
+				<Input
+					value={inviteLink}
+					color="blue.400"
 					onClick={onCopy}
-					size="md"
-					variant="ghost"
+					isReadOnly
 				/>
-			</InputRightElement>
-		</InputGroup>
+				<InputRightElement>
+					<IconButton
+						icon={<CopyIcon color="gray.500" />}
+						aria-label="Copy invite link"
+						onClick={onCopy}
+						size="md"
+						variant="ghost"
+					/>
+				</InputRightElement>
+			</InputGroup>
+			<Button
+				colorScheme="gray"
+				variant="subtle"
+				ml={4}
+				border="1px solid"
+				borderColor={useColorModeValue('gray.200', 'gray.700')}
+				onClick={() => {
+					refreshInviteLink();
+				}}
+			>
+				Refresh link
+			</Button>
+		</Flex>
 	);
 };
 
