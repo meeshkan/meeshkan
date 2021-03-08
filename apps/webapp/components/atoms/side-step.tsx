eb7d83e57b428a2927183f1e86f44f4f5bef1409
Makenna Smutz <makennasmutz@gmail.com>
2021-03-08 11:19:26 +0100

    Fix line styles

--- apps/webapp/components/atoms/side-step.tsx
+++ apps/webapp/components/atoms/side-step.tsx
@@ -28,53 +28,51 @@ export const SideStep = ({
 				backgroundColor: useColorModeValue('white', 'gray.900'),
 			}}
 		>
-			{stepNumber === 1 ? null : (
-				<Box borderLeft="1px solid" borderColor="gray.300" h={4} ml="11.5px" />
-			)}
 			<Flex>
-				<Flex
-					justify="center"
-					align="center"
-					borderRadius="full"
-					h={6}
-					w={6}
-					border="1px solid"
-					borderColor="gray.500"
-					fontWeight="500"
-					mr={4}
-				>
-					{stepNumber}
-				</Flex>
-				<Text
-					fontWeight="700"
-					lineHeight="1.4"
-					color={
-						stepName.startsWith('Untitled')
-							? useColorModeValue('gray.400', 'gray.500')
-							: 'inherit'
-					}
-				>
+				<Box>
+					<Flex
+						justify="center"
+						align="center"
+						borderRadius="full"
+						h={6}
+						w={6}
+						border="1px solid"
+						borderColor="gray.500"
+						fontWeight="500"
+						fontSize="sm"
+						mr={4}
+					>
+						{stepNumber}
+					</Flex>
+					<Box
+						borderLeft="1px solid"
+						borderColor="gray.300"
+						h="calc(100% - 24px)"
+						ml="11.5px"
+					/>
+				</Box>
+				<Text flex="1" fontWeight="500" lineHeight="1.4" fontSize="md" pb={4}>
 					{stepName}
 				</Text>
 			</Flex>
-
-			<List
-				styleType="disc"
-				stylePosition="inside"
-				pl={6}
-				pb={4}
-				spacing={2}
-				ml="11.5px"
-				borderLeft="1px solid"
-				borderColor="gray.300"
-			>
-				{subSteps &&
-					subSteps.map((step) => (
+			{subSteps && (
+				<List
+					styleType="disc"
+					stylePosition="inside"
+					pl={6}
+					pb={4}
+					spacing={2}
+					ml="11.5px"
+					borderLeft="1px solid"
+					borderColor="gray.300"
+				>
+					{subSteps.map((step) => (
 						<ListItem key={step.sIndex} lineHeight="1.6">
 							{step.text}
 						</ListItem>
 					))}
-			</List>
+				</List>
+			)}
 		</Box>
 	);
 };
