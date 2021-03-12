e73d7e591119163348238fc93a0c70aa172bf3ec
Mike Solomon <mike@meeshkan.com>
2021-03-12 08:00:55 +0200

    Finishes function draft 1

--- libs/downloadable-script/src/lib/downloadable-script.ts
+++ libs/downloadable-script/src/lib/downloadable-script.ts
@@ -2,11 +2,6 @@ import { SeleniumScript } from '@frontend/meeshkan-types';
 import { Either, fold, left, right } from 'fp-ts/Either';
 import { flow } from 'fp-ts/lib/function';
 
-function notEmpty<SeleniumCommand>(
-	value: SeleniumCommand | null | undefined
-): value is SeleniumCommand {
-	return value !== null && value !== undefined;
-}
 interface ScriptTargetSelector {
 	xpath: string;
 	selector?: string;
@@ -45,16 +40,16 @@ const dragndrop: (o: Dragndrop) => ScriptCommand = flow(
 	right
 );
 
-const scriptPatternMatch = <T>(
-	open: (o: ScriptOpen) => T,
-	setViewportSize: (s: ScriptSetViewportSize) => T,
-	click: (c: Click) => T,
-	type: (t: Type) => T,
-	dragndrop: (d: Dragndrop) => T
-) => (command: ScriptCommand): T =>
+const transformCommand = <T>(c: {
+	open: (o: ScriptOpen) => T;
+	setViewportSize: (s: ScriptSetViewportSize) => T;
+	click: (c: Click) => T;
+	type: (t: Type) => T;
+	dragndrop: (d: Dragndrop) => T;
+}) => (command: ScriptCommand): T =>
 	fold(
-		open,
-		fold(setViewportSize, fold(click, fold(type, dragndrop)))
+		c.open,
+		fold(c.setViewportSize, fold(c.click, fold(c.type, c.dragndrop)))
 	)(command);
 
 interface ScriptOpen {
@@ -76,19 +71,22 @@ interface Dragndrop {
 	destinationTarget: ScriptTarget;
 }
 
-const eightBaseToSeleniumScript = (
+const n2u = <T>(i: T | null | undefined): T | undefined =>
+	i === null ? undefined : i;
+function isInhabited<T>(value: T | null | undefined): value is T {
+	return value !== null && value !== undefined;
+}
+export const eightBaseToScript = (
 	script: SeleniumScript
-): Array<ScriptCommand> =>
+): Array<ScriptCommand> | undefined =>
 	script?.groups?.items
 		?.map((group) =>
 			group?.commands?.items?.map((command) =>
-				command.open && command.open.value
+				command?.open?.value
 					? open({
 							value: command.open.value,
 					  })
-					: command.setViewportSize &&
-					  command.setViewportSize.value &&
-					  command.setViewportSize.value.xCoord &&
+					: command?.setViewportSize?.value?.xCoord &&
 					  command.setViewportSize.value.yCoord
 					? setViewportSize({
 							value: {
@@ -96,24 +94,20 @@ const eightBaseToSeleniumScript = (
 								yCoord: command.setViewportSize.value.yCoord,
 							},
 					  })
-					: command.click &&
-					  command.click.target &&
-					  command.click.target.selector &&
-					  command.click.target.selector.xpath &&
+					: command?.click?.target?.selector?.xpath &&
 					  command.click.target.selector.tagName
 					? click({
 							target: {
 								selector: {
 									xpath: command.click.target.selector.xpath,
-									selector: command.click.target.selector.selector,
-									className: command.click.target.selector.className,
+									selector: n2u(command.click.target.selector.selector),
+									className: n2u(command.click.target.selector.className),
 									tagName: command.click.target.selector.tagName,
-									tagId: command.click.target.selector.tagId,
-									innerText: command.click.target.selector.innerText,
+									tagId: n2u(command.click.target.selector.tagId),
+									innerText: n2u(command.click.target.selector.innerText),
 								},
 								coordinates:
-									command.click.target.coordinates &&
-									command.click.target.coordinates.xCoord &&
+									command?.click?.target?.coordinates?.xCoord &&
 									command.click.target.coordinates.yCoord
 										? {
 												xCoord: command.click.target.coordinates.xCoord,
@@ -122,26 +116,22 @@ const eightBaseToSeleniumScript = (
 										: undefined,
 							},
 					  })
-					: command.type &&
-					  command.type.target &&
-					  command.type.target.selector &&
-					  command.type.target.selector.xpath &&
-					  command.type.value &&
-					  command.type.target.selector.tagName
+					: command?.type?.target?.selector?.xpath &&
+					  command?.type?.target?.selector?.tagName &&
+					  command?.type?.value
 					? type({
 							value: command.type.value,
 							target: {
 								selector: {
 									xpath: command.type.target.selector.xpath,
-									selector: command.type.target.selector.selector,
-									className: command.type.target.selector.className,
+									selector: n2u(command.type.target.selector.selector),
+									className: n2u(command.type.target.selector.className),
 									tagName: command.type.target.selector.tagName,
-									tagId: command.type.target.selector.tagId,
-									innerText: command.type.target.selector.innerText,
+									tagId: n2u(command.type.target.selector.tagId),
+									innerText: n2u(command.type.target.selector.innerText),
 								},
 								coordinates:
-									command.type.target.coordinates &&
-									command.type.target.coordinates.xCoord &&
+									command?.type?.target?.coordinates?.xCoord &&
 									command.type.target.coordinates.yCoord
 										? {
 												xCoord: command.type.target.coordinates.xCoord,
@@ -150,28 +140,28 @@ const eightBaseToSeleniumScript = (
 										: undefined,
 							},
 					  })
-					: command.dragndrop &&
-					  command.dragndrop.sourceTarget &&
-					  command.dragndrop.sourceTarget.selector &&
-					  command.dragndrop.sourceTarget.selector.xpath &&
-					  command.dragndrop.sourceTarget.selector.tagName &&
-					  command.dragndrop.destinationTarget &&
-					  command.dragndrop.destinationTarget.selector &&
-					  command.dragndrop.destinationTarget.selector.xpath &&
-					  command.dragndrop.destinationTarget.selector.tagName
+					: command?.dragndrop?.sourceTarget?.selector?.xpath &&
+					  command?.dragndrop?.sourceTarget?.selector?.tagName &&
+					  command?.dragndrop?.destinationTarget?.selector?.xpath &&
+					  command?.dragndrop?.destinationTarget?.selector?.tagName
 					? dragndrop({
 							sourceTarget: {
 								selector: {
 									xpath: command.dragndrop.sourceTarget.selector.xpath,
-									selector: command.dragndrop.sourceTarget.selector.selector,
-									className: command.dragndrop.sourceTarget.selector.className,
+									selector: n2u(
+										command.dragndrop.sourceTarget.selector.selector
+									),
+									className: n2u(
+										command.dragndrop.sourceTarget.selector.className
+									),
 									tagName: command.dragndrop.sourceTarget.selector.tagName,
-									tagId: command.dragndrop.sourceTarget.selector.tagId,
-									innerText: command.dragndrop.sourceTarget.selector.innerText,
+									tagId: n2u(command.dragndrop.sourceTarget.selector.tagId),
+									innerText: n2u(
+										command.dragndrop.sourceTarget.selector.innerText
+									),
 								},
 								coordinates:
-									command.dragndrop.sourceTarget.coordinates &&
-									command.dragndrop.sourceTarget.coordinates.xCoord &&
+									command?.dragndrop?.sourceTarget?.coordinates?.xCoord &&
 									command.dragndrop.sourceTarget.coordinates.yCoord
 										? {
 												xCoord:
@@ -184,18 +174,22 @@ const eightBaseToSeleniumScript = (
 							destinationTarget: {
 								selector: {
 									xpath: command.dragndrop.destinationTarget.selector.xpath,
-									selector:
-										command.dragndrop.destinationTarget.selector.selector,
-									className:
-										command.dragndrop.destinationTarget.selector.className,
+									selector: n2u(
+										command.dragndrop.destinationTarget.selector.selector
+									),
+									className: n2u(
+										command.dragndrop.destinationTarget.selector.className
+									),
 									tagName: command.dragndrop.destinationTarget.selector.tagName,
-									tagId: command.dragndrop.destinationTarget.selector.tagId,
-									innerText:
-										command.dragndrop.destinationTarget.selector.innerText,
+									tagId: n2u(
+										command.dragndrop.destinationTarget.selector.tagId
+									),
+									innerText: n2u(
+										command.dragndrop.destinationTarget.selector.innerText
+									),
 								},
 								coordinates:
-									command.dragndrop.destinationTarget.coordinates &&
-									command.dragndrop.destinationTarget.coordinates.xCoord &&
+									command?.dragndrop?.destinationTarget?.coordinates?.xCoord &&
 									command.dragndrop.destinationTarget.coordinates.yCoord
 										? {
 												xCoord:
@@ -211,5 +205,69 @@ const eightBaseToSeleniumScript = (
 					: undefined
 			)
 		)
-		?.filter(notEmpty)
-		?.reduce((a, b) => [...a, ...b], []);
+		?.filter(isInhabited)
+		?.reduce((a, b) => [...a, ...b], [])
+		?.filter(isInhabited);
+
+const topMatterPptr = (
+	headless: boolean
+): string => `const puppeteer = require('puppeteer');
+(async () => {
+  const browser = await puppeteer.launch({headless: ${headless}});
+	const page = await browser.newPage();
+	let ddSource;
+	let ddDestination;
+	let ddSourceBB;
+	let ddDestinationBB;`;
+const bottomMatterPptr = `
+  await browser.close();
+})();`;
+interface ScriptToPptrOptions {
+	headless: boolean;
+}
+export const scriptToPptr = (
+	script: Array<ScriptCommand>,
+	options: ScriptToPptrOptions
+): string =>
+	[
+		[topMatterPptr(options.headless)],
+		script.map(
+			transformCommand({
+				open: ({ value }) => `  page.goto(${JSON.stringify(value)});`,
+				setViewportSize: ({ value: { xCoord, yCoord } }) =>
+					`  await page.setViewport({ width: ${xCoord}, height: ${yCoord}, deviceScaleFactor: 1 });`,
+				click: ({
+					target: {
+						selector: { xpath },
+					},
+				}) => `  await (await page.$x(${JSON.stringify(xpath)}))[0].click();`,
+				type: ({
+					target: {
+						selector: { xpath },
+					},
+					value,
+				}) =>
+					`  await (await page.$x(${JSON.stringify(
+						xpath
+					)}))[0].type(${JSON.stringify(value)}, {delay: 100});`,
+				dragndrop: ({
+					sourceTarget,
+					destinationTarget,
+				}) => `  ddSource = (await page.$x(${JSON.stringify(
+					sourceTarget.selector.xpath
+				)}))[0];
+	ddDestination = (await page.$x(${JSON.stringify(
+		destinationTarget.selector.xpath
+	)}))[0];
+  ddSourceBB = await ddSource.boundingBox();			
+  ddDestinationBB = await ddDestination.boundingBox();
+	await page.mouse.move(ddSourceBB.x + ddSourceBB.width / 2, ddSourceBB.y + ddSourceBB.height / 2);
+	await page.mouse.down();
+	await page.mouse.move(ddDestinationBB.x + ddDestinationBB.width / 2, ddDestinationBB.y + ddDestinationBB.height / 2);
+	await page.mouse.up();`,
+			})
+		),
+		[bottomMatterPptr],
+	]
+		.reduce((a, b) => [...a, ...b], [])
+		.reduce((a, b) => a + '\n' + b, '');
