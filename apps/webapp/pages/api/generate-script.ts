f9bf7696095305f492f4be59801b932e4ec0c12e
Mike Solomon <mike@meeshkan.com>
2021-03-12 08:04:41 +0200

    Sets up endpoint

--- apps/webapp/pages/api/generate-script.ts
+++ apps/webapp/pages/api/generate-script.ts
@@ -1,12 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-
+import { eightBaseToScript, scriptToPptr } from '@frontend/downloadable-script';
 export default async function generateScript(
 	req: NextApiRequest,
 	res: NextApiResponse
 ): Promise<void> {
 	try {
-		res.json({ message: 'Generating script...' });
-		// TODO: add script generation logic here
+		res.json({
+			script: scriptToPptr(eightBaseToScript(req.body.script), {
+				headless: false,
+			}),
+		});
 	} catch (error) {
 		console.error(error);
 		res.status(error.status || 500).end(error.message);
