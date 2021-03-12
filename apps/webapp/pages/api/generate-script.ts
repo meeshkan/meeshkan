b392bede8b32b1d23bb5369c362883f042ffa7ce
Mike Solomon <mike@meeshkan.com>
2021-03-12 09:20:14 +0200

    Fixes api endpoint syntax

--- apps/webapp/pages/api/generate-script.ts
+++ apps/webapp/pages/api/generate-script.ts
@@ -1,12 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { eightBaseToScript, scriptToPptr } from '@frontend/downloadable-script';
+import { eightBaseToPptr } from '@frontend/downloadable-script';
 export default async function generateScript(
 	req: NextApiRequest,
 	res: NextApiResponse
 ): Promise<void> {
 	try {
 		res.json({
-			script: scriptToPptr(eightBaseToScript(req.body.script), {
+			script: eightBaseToPptr(req.body.script, {
 				headless: false,
 			}),
 		});
