44e01b9bb175ec0c31ea579296fd3b87be555d48
Nikolaos Kamarinakis <nikolaskam@gmail.com>
2021-03-26 09:56:40 +0200

    Use native `Date` instead of moment.js (#119)

--- apps/webapp/utils/date.ts
+++ apps/webapp/utils/date.ts
@@ -4,3 +4,24 @@ export const getDateInEightBaseFormat = (date: Date): string => {
     const dd = date.getDate();
     return `${yyyy}-${(mm > 9 ? '' : '0') + mm}-${(dd > 9 ? '' : '0') + dd}`;
 };
+
+export const daysUntilDate = (date: Date): number =>
+	Math.ceil((new Date().getTime() - date.getTime()) / (1000 * 3600 * 24));
+
+export const lastNDays = (n: number): Date[] => {
+	return [...Array(n).keys()]
+		.map((i) => {
+			const date = new Date();
+			date.setDate(date.getDate() - i);
+			return date;
+		})
+		.reverse();
+};
+
+export const isSameDay = (dateA: Date, dateB: Date): boolean => {
+	return (
+		dateA.getDate() === dateB.getDate() &&
+		dateA.getMonth() === dateB.getMonth() &&
+		dateA.getFullYear() === dateB.getFullYear()
+	);
+};
