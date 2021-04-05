e050129c1d187c6155c70bdde793a9e33b3ea6eb
Makenna Smutz <makennasmutz@gmail.com>
2021-04-05 18:35:38 +0200

    fix type error

--- libs/chakra-theme/src/lib/components.tsx
+++ libs/chakra-theme/src/lib/components.tsx
@@ -107,6 +107,40 @@ const customComponents = {
 			zIndex: 'tooltip',
 		}),
 	},
+	Menu: {
+		parts: ['list', 'item'],
+		baseStyle: (props: GlobalStyleProps) => ({
+			list: {
+				bg: mode(`#fff`, `gray.700`)(props),
+				boxShadow: mode(`sm`, `dark-lg`)(props),
+				color: 'inherit',
+				minW: '3xs',
+				p: '2',
+				zIndex: 1,
+				borderRadius: 'md',
+				borderWidth: '1px',
+			},
+			item: {
+				py: '0.4rem',
+				px: '0.8rem',
+				transition: 'background 50ms ease-in 0s',
+				borderRadius: 'md',
+				_focus: {
+					bg: mode(`gray.100`, `whiteAlpha.100`)(props),
+				},
+				_active: {
+					bg: mode(`gray.200`, `whiteAlpha.200`)(props),
+				},
+				_expanded: {
+					bg: mode(`gray.100`, `whiteAlpha.100`)(props),
+				},
+				_disabled: {
+					opacity: 0.4,
+					cursor: 'not-allowed',
+				},
+			},
+		}),
+	},
 };
 
 export default customComponents;
