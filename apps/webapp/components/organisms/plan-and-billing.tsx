7371e84cd7f53360666c56e433ee6ca6c70c9f27
Makenna Smutz <makennasmutz@gmail.com>
2021-04-13 02:45:15 +0200

    Finish styling of billing page

--- apps/webapp/components/organisms/plan-and-billing.tsx
+++ apps/webapp/components/organisms/plan-and-billing.tsx
@@ -1,9 +1,31 @@
 import React, { useState, useContext } from 'react';
-import { Button, Text, Box, Stack, LightMode } from '@chakra-ui/react';
+import {
+	Button,
+	Text,
+	Box,
+	Stack,
+	LightMode,
+	ListIcon,
+	ListItem,
+	List,
+	Flex,
+	useColorModeValue,
+	Code,
+	FormControl,
+	Input,
+	Tooltip,
+	Popover,
+	PopoverTrigger,
+	PopoverContent,
+	PopoverArrow,
+	PopoverCloseButton,
+} from '@chakra-ui/react';
 import GridCard from '../molecules/grid-card';
-import { useRouter } from 'next/router';
 import { UserContext } from '../../utils/user';
+import { Plans } from '../../utils/stripe';
 import { getStripe } from '../../utils/stripe-client';
+import { CheckSquareIcon } from '@frontend/chakra-theme';
+import SegmentedControl from '../molecules/segmented-control';
 
 const PlanAndBillingCard = () => {
 	const [billingInterval, setBillingInterval] = useState('month');
@@ -51,66 +73,246 @@ const PlanAndBillingCard = () => {
 		}
 	};
 
+	const [toggleIndex, setToggleIndex] = useState(0);
+
+	const iconBlue = useColorModeValue('blue.500', 'blue.300');
+	const codeBg = useColorModeValue('blue.50', 'gray.800');
+	const borderGray = useColorModeValue('gray.100', 'gray.800');
+	const tertiaryText = useColorModeValue('gray.300', 'gray.700');
+	const { free, feedback, business } = Plans;
+
 	return (
 		<GridCard
 			anchor
 			title="Plan and Billing"
 			subtitle="Information about the plan you're on and Billing powered by Stripe."
 		>
+			<Flex justify="center" align="center" mb={6}>
+				<Text mr={4} fontWeight="600">
+					Billing
+				</Text>
+				<SegmentedControl
+					values={['Monthly', 'Yearly -20%']}
+					selectedIndex={toggleIndex}
+					setSelectedIndex={setToggleIndex}
+				/>
+			</Flex>
 			<Stack direction="row" w="full" spacing={8}>
+				<Flex direction="column" align="center" w="full">
+					<Code
+						variant="outline"
+						background={codeBg}
+						h="20px"
+						borderRadius="md"
+						py={2}
+						px={4}
+						colorScheme="blue"
+						maxW="fit-content"
+						mb="-10px"
+						zIndex="1"
+					>
+						Most popular
+					</Code>
+					<Box
+						p={4}
+						w="full"
+						borderRadius="lg"
+						border="1px solid"
+						borderColor={iconBlue}
+						boxShadow="0px 8px 24px 0px rgba(149,157,165,0.2)"
+					>
+						<Flex justify="space-between" mb={4}>
+							<Text fontSize="24px" fontWeight="800">
+								Feedback
+							</Text>
+							<Flex>
+								<Text
+									fontWeight="800"
+									fontSize="24px"
+									mr={4}
+									textDecor="line-through"
+									color={tertiaryText}
+								>
+									{toggleIndex === 0
+										? feedback.monthlyPrice
+										: feedback.yearlyPrice}
+								</Text>
+								<Text fontWeight="800" fontSize="24px">
+									{toggleIndex === 0
+										? feedback.discountedMonthly
+										: feedback.discountedYearly}
+								</Text>
+							</Flex>
+						</Flex>
+						<Text textAlign="center" color="gray.500" mb={8}>
+							{feedback.description}
+						</Text>
+						<List
+							spacing={2}
+							sx={{ '-webkit-columns': '2', '-moz-columns': '2', columns: 2 }}
+						>
+							{feedback.features.map((feature) => (
+								<ListItem lineHeight="1.2" fontSize="14px">
+									<ListIcon as={CheckSquareIcon} color={iconBlue} />
+									{feature}
+								</ListItem>
+							))}
+						</List>
+						<Flex direction="column" justify="center" align="center" mt={8}>
+							<Popover trigger="hover" placement="top">
+								<LightMode>
+									<PopoverTrigger>
+										<Button
+											w="full"
+											isLoading={priceIdLoading}
+											loadingText="Loading stripe"
+											onClick={() =>
+												handleCheckout(
+													toggleIndex === 0
+														? feedback.monthlyPriceId
+														: feedback.yearlyPriceId
+												)
+											}
+										>
+											Choose the Feedback plan
+										</Button>
+									</PopoverTrigger>
+								</LightMode>
+								<PopoverContent p={3}>
+									<PopoverArrow />
+									<Flex as="span" align="center">
+										Use code{' '}
+										<Code
+											ml={2}
+											px={2}
+											py={1}
+											borderRadius="md"
+											variant="outline"
+											fontSize="inherit"
+											colorScheme="red"
+										>
+											Feedback100
+										</Code>
+									</Flex>
+									<PopoverCloseButton mt="3px" />
+								</PopoverContent>
+							</Popover>
+						</Flex>
+					</Box>
+				</Flex>
 				<Box
+					d="flex"
+					flexDirection="column"
+					justifyContent="space-between"
 					p={4}
-					textAlign="center"
 					w="full"
 					borderRadius="lg"
 					border="1px solid"
-					borderColor="gray.200"
+					borderColor="transparent"
+					boxShadow="0px 8px 24px 0px rgba(149,157,165,0.2)"
 				>
-					<Text mb={8}>Feedback</Text>{' '}
-					<LightMode>
+					<Box>
+						<Flex justify="space-between" mb={4}>
+							<Text fontSize="24px" fontWeight="800">
+								Business
+							</Text>
+							<Flex>
+								<Text fontWeight="800" fontSize="24px">
+									{toggleIndex === 0
+										? business.monthlyPrice
+										: business.yearlyPrice}
+								</Text>
+							</Flex>
+						</Flex>
+						<Text textAlign="center" color="gray.500" mb={8}>
+							{business.description}
+						</Text>
+						<List
+							spacing={2}
+							sx={{ '-webkit-columns': '2', '-moz-columns': '2', columns: 2 }}
+						>
+							{business.features.map((feature) => (
+								<ListItem lineHeight="1.2" fontSize="14px">
+									<ListIcon as={CheckSquareIcon} color={iconBlue} />
+									{feature}
+								</ListItem>
+							))}
+						</List>
+					</Box>
+					<Flex justify="center">
 						<Button
+							variant="subtle"
+							mt={8}
+							w="full"
+							loadingText="Loading stripe"
 							isLoading={priceIdLoading}
-							// Test mode feedback monthly
-							onClick={() => handleCheckout(`price_1If50lA2WCpbIMtYnlSVlBFo`)}
+							onClick={() =>
+								handleCheckout(
+									toggleIndex === 0
+										? business.monthlyPriceId
+										: business.yearlyPriceId
+								)
+							}
 						>
-							Choose the feedback plan
+							Choose the Business plan
 						</Button>
-					</LightMode>
-				</Box>
-				<Box
-					p={4}
-					textAlign="center"
-					w="full"
-					borderRadius="lg"
-					border="1px solid"
-					borderColor="gray.200"
-				>
-					<Text mb={8}>Business</Text>{' '}
-					<LightMode>
-						<Button
-							// isLoading={priceIdLoading}
-							// Free monthly hardcoded
-							onClick={() => handleCheckout(`price_1IeQD9A2WCpbIMtYuC0KZchg`)}
-						>
-							Choose the business plan
-						</Button>
-					</LightMode>
+					</Flex>
 				</Box>
 			</Stack>
-			<Box
+			<Stack
+				w="full"
+				direction="row"
+				align="center"
+				justifyContent="space-between"
 				p={4}
 				mt={8}
-				textAlign="center"
-				w="full"
 				borderRadius="lg"
 				border="1px solid"
-				borderColor="gray.200"
+				borderColor={borderGray}
 			>
-				<Text mb={8}>Free</Text>{' '}
-				<LightMode>
-					<Button>Wait for the free plan to be available</Button>
-				</LightMode>
-			</Box>
+				<Flex align="baseline">
+					<Text fontSize="24px" fontWeight="800" mr={4}>
+						Free
+					</Text>
+					<Text color="gray.500">{free.description}</Text>
+				</Flex>
+				<Flex
+					w="full"
+					as="form"
+					maxW="600px"
+					mx={['none', 'none', 'auto']}
+					border="1px solid"
+					borderColor={useColorModeValue('gray.300', 'gray.700')}
+					borderRadius="xl"
+					p={2}
+					transition="all 0.2s"
+					_hover={{ borderColor: useColorModeValue('gray.400', 'gray.600') }}
+					_focusWithin={{
+						borderColor: useColorModeValue('blue.400', 'blue.600'),
+					}}
+					direction={['column', 'column', 'row']}
+				>
+					<FormControl isRequired>
+						<Input
+							type="email"
+							name="email"
+							id="email"
+							defaultValue={user?.email}
+							placeholder="shipit@meeshkan.com"
+							_placeholder={{
+								color: useColorModeValue('gray.500', 'gray.400'),
+							}}
+							mr={4}
+							border="none"
+							_focus={{}}
+							mb={[4, 4, 0]}
+						/>
+					</FormControl>
+					<Button minW="fit-content" type="submit" variant="subtle">
+						Subscribe to updates
+					</Button>
+				</Flex>
+			</Stack>
 		</GridCard>
 	);
 };
--- apps/webapp/utils/stripe.ts
+++ apps/webapp/utils/stripe.ts
@@ -58,30 +58,54 @@ export const createOrRetrieveCustomer = async ({
 
 // Plans
 
-export const Plans = [
-	{
-		free: {
-			// @ts-expect-error
-			monthlyPriceId: null,
-			// @ts-expect-error
-			yearlyPriceId: null,
-			features: [''],
-		},
+export const Plans = {
+	free: {
+		monthlyPrice: '0€',
+		yearlyPrice: '0€',
+		description: 'Wait and be notified when a free plan is available.',
+		// @ts-expect-error
+		monthlyPriceId: null,
+		// @ts-expect-error
+		yearlyPriceId: null,
+		features: [''],
 	},
-	{
-		feedback: {
-			monthlyPriceId: `price_1IeQD9A2WCpbIMtYuC0KZchg`,
-			yearlyPriceId: `price_1IXxTbA2WCpbIMtY8hvr4kgS`,
-			features: [''],
-		},
+
+	feedback: {
+		monthlyPrice: '45€',
+		yearlyPrice: '432€',
+		discountedMonthly: '0€',
+		discountedYearly: '0€',
+		description:
+			'Have a 30 minute call fortnightly with the Meeshkan team to offer insight into your workflow.',
+		monthlyPriceId: `price_1IfZ4BA2WCpbIMtY7s89W6J2`,
+		yearlyPriceId: `price_1IfZ4ZA2WCpbIMtYVPbGvJGc`,
+		features: [
+			'User generated tests',
+			'30 test runs per month',
+			'No-code test creation',
+			'Unlimited team members',
+			'5 Concurrent Tests',
+			'3 month data retention',
+		],
 	},
-	{
-		business: {
-			// 29.99
-			monthlyPriceId: `price_1IXxUAA2WCpbIMtYgVTV4xzh`,
-			// 20% discount 288.00
-			yearlyPriceId: `price_1IbrAcA2WCpbIMtYJKWdTVAM`,
-			features: [''],
-		},
+
+	business: {
+		monthlyPrice: '75€',
+		yearlyPrice: '720€',
+		description:
+			'This is the perfect plan if you’re a team looking to get some serious UI-testing done.',
+		// 29.99
+		monthlyPriceId: `price_1IfZ2jA2WCpbIMtYL4iWIbvg`,
+		// 20% discount 288.00
+		yearlyPriceId: `price_1IfZ2yA2WCpbIMtYJludWHEh`,
+		features: [
+			'User generated tests',
+			'100+ test runs per month',
+			'No-code test creation',
+			'Unlimited team members',
+			'30+ Concurrent Tests',
+			'9 month data retention',
+			'Video of test cases and outcomes',
+		],
 	},
-];
+};
