f17584b805bf8d31fec69e6d968b5b2f2edf0246
Makenna Smutz <makennasmutz@gmail.com>
2021-04-19 17:55:47 +0200

    Start adding plan/billing into onboarding

--- apps/webapp/components/organisms/create-project.tsx
+++ apps/webapp/components/organisms/create-project.tsx
@@ -3,9 +3,11 @@ import Router from 'next/router';
 import { Heading, Box, Flex, Button } from '@chakra-ui/react';
 import Card from '../atoms/card';
 import CreateProjectForm from '../molecules/create-project-form';
+import PlanAndBillingCard from './plan-and-billing';
 
 const CreateProject = () => {
 	const [loading, setLoading] = useState(false);
+	const [stepNumber, setStepNumber] = useState(2);
 	return (
 		<Flex
 			as={Card}
@@ -17,20 +19,41 @@ const CreateProject = () => {
 			p={8}
 		>
 			<>
-				<Box>
-					<Heading as="h1" fontSize="3xl" mb={8} textAlign="center">
-						Create new project
-					</Heading>
-					<CreateProjectForm setLoading={setLoading} />
-				</Box>
-				<Flex justify="space-between" align="center" w="100%">
-					<Button mt={4} colorScheme="gray" onClick={() => Router.push('/')}>
-						Back
-					</Button>
-					<Button mt={4} type="submit" isLoading={loading} form="form">
-						Create project
-					</Button>
-				</Flex>
+				{stepNumber === 1 ? (
+					<Box>
+						<Heading as="h1" fontSize="3xl" mb={8} textAlign="center">
+							Create new project
+						</Heading>
+						<CreateProjectForm setLoading={setLoading} />
+					</Box>
+				) : stepNumber === 2 ? (
+					<Box>
+						<Heading as="h1" fontSize="3xl" mb={8} textAlign="center">
+							Choose your plan
+						</Heading>
+						<PlanAndBillingCard />
+						<Flex justify="space-between" align="center" w="100%">
+							<Button
+								mt={4}
+								colorScheme="gray"
+								onClick={() => Router.push('/')}
+							>
+								Back
+							</Button>
+							<Button
+								mt={4}
+								type="submit"
+								isLoading={loading}
+								loadingText="Creating project"
+								form="form"
+							>
+								Create project
+							</Button>
+						</Flex>
+					</Box>
+				) : (
+					'Error'
+				)}
 			</>
 		</Flex>
 	);
