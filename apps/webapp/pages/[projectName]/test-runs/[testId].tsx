b2396280d4bb516c7d0d4abe2a72a47adeac6899
Makenna Smutz <makennasmutz@gmail.com>
2021-04-30 12:03:36 +0200

    fix wrong conflict resolution

--- apps/webapp/pages/[projectName]/test-runs/[testId].tsx
+++ apps/webapp/pages/[projectName]/test-runs/[testId].tsx
@@ -330,62 +330,62 @@ const TestRun = () => {
 									})}
 								</Accordion>
 							</Stack>
-							<GridCard title="Technical information">
-								<Stack spacing={5}>
-									<Box>
-										<Heading
-											as="h3"
-											fontSize="15px"
-											fontWeight="600"
-											lineHeight="short"
-										>
-											Test length
-										</Heading>
-										<Text fontSize="15px">
-											{testRun?.testLength.substring(0, 2) +
-												` hrs, ` +
-												testRun?.testLength.substring(3, 5) +
-												` mins, ` +
-												testRun?.testLength.substring(6, 8) +
-												` sec ` || '-'}
-										</Text>
-									</Box>
-									<Box>
-										<Heading
-											as="h3"
-											fontSize="15px"
-											fontWeight="600"
-											lineHeight="short"
-										>
-											Web browser
-										</Heading>
-										<Text fontSize="15px">Chromium 86.0.4240.0</Text>
-									</Box>
-									<Box>
-										<Heading
-											as="h3"
-											fontSize="15px"
-											fontWeight="600"
-											lineHeight="short"
-										>
-											Operating system
-										</Heading>
-										<Text fontSize="15px">AWS Linux 2018.03</Text>
-									</Box>
-									<Box>
-										<Heading
-											as="h3"
-											fontSize="15px"
-											fontWeight="600"
-											lineHeight="short"
-										>
-											Language
-										</Heading>
-										<Text fontSize="15px">English</Text>
-									</Box>
-								</Stack>
-							</GridCard>
 						</Stack>
+						<GridCard title="Technical information">
+							<Stack spacing={5}>
+								<Box>
+									<Heading
+										as="h3"
+										fontSize="15px"
+										fontWeight="600"
+										lineHeight="short"
+									>
+										Test length
+									</Heading>
+									<Text fontSize="15px">
+										{testRun?.testLength.substring(0, 2) +
+											` hrs, ` +
+											testRun?.testLength.substring(3, 5) +
+											` mins, ` +
+											testRun?.testLength.substring(6, 8) +
+											` sec ` || '-'}
+									</Text>
+								</Box>
+								<Box>
+									<Heading
+										as="h3"
+										fontSize="15px"
+										fontWeight="600"
+										lineHeight="short"
+									>
+										Web browser
+									</Heading>
+									<Text fontSize="15px">Chromium 86.0.4240.0</Text>
+								</Box>
+								<Box>
+									<Heading
+										as="h3"
+										fontSize="15px"
+										fontWeight="600"
+										lineHeight="short"
+									>
+										Operating system
+									</Heading>
+									<Text fontSize="15px">AWS Linux 2018.03</Text>
+								</Box>
+								<Box>
+									<Heading
+										as="h3"
+										fontSize="15px"
+										fontWeight="600"
+										lineHeight="short"
+									>
+										Language
+									</Heading>
+									<Text fontSize="15px">English</Text>
+								</Box>
+							</Stack>
+						</GridCard>
 					</Flex>
 				</Box>
 			</Stack>
