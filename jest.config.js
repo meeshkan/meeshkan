6007eb8c50c30848192f6899c9c6dc62d48f1f97
Nikolaos Kamarinakis <nikolaskam@gmail.com>
2021-06-29 15:54:45 +0300

    Remove website (#193)
    * Remove website
    
    * remove a few more packages
    
    Co-authored-by: Makenna Smutz <makennasmutz@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
 	preset: 'jest-puppeteer',
 	projects: [
 		'<rootDir>/apps/webapp',
-		'<rootDir>/apps/website',
 		'<rootDir>/libs/chakra-theme',
 		'<rootDir>/apps/og-cards',
 		'<rootDir>/libs/meeshkan-types',
