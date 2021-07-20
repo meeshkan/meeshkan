451993098efcab89703549ab4676cd0a7d0d876d
Makenna Smutz <makennasmutz@gmail.com>
2021-07-20 10:25:36 +0200

    initial onboarding steps setup

--- apps/webapp/components/organisms/create-project.tsx
+++ apps/webapp/components/organisms/create-project.tsx
@@ -1,11 +1,21 @@
 import { useState } from 'react';
 import Router from 'next/router';
-import { Heading, Box, Flex, Button } from '@chakra-ui/react';
+import { Heading, Box, Flex, Button, Text } from '@chakra-ui/react';
 import Card from '../atoms/card';
 import CreateProjectForm from '../molecules/create-project-form';
 
 const CreateProject = () => {
+	const [step, setStep] = useState<1 | 2 | 3>(1);
 	const [loading, setLoading] = useState(false);
+
+	const isFirstStep = step === 1;
+	const title = isFirstStep
+		? 'Create a project'
+		: step === 2
+			? 'How would you like to create test cases?'
+			: 'At what cadence would you like test runs to happen?';
+	const submitButtonText = step === 3 ? 'Finish' : 'Next step';
+
 	return (
 		<Flex
 			as={Card}
@@ -18,14 +28,21 @@ const CreateProject = () => {
 		>
 			<Box>
 				<Heading as="h1" fontSize="3xl" mb={8} textAlign="center">
-					Create new project
+					Onboarding — {title}
 				</Heading>
-				<CreateProjectForm setLoading={setLoading} />
+				{step === 1 ? (
+					<CreateProjectForm setLoading={setLoading} /*setStep*/ />
+				) : step === 2 ? (
+					<Box>Create test cases</Box>
+				) : (
+					<Box>Test run cadence</Box>
+				)}
 			</Box>
 			<Flex justify="space-between" align="center" w="100%">
 				<Button mt={4} colorScheme="gray" onClick={() => Router.back()}>
 					Back
 				</Button>
+				<Text color="gray.500">Step {step} of 3</Text>
 				<Button
 					mt={4}
 					type="submit"
@@ -33,7 +50,7 @@ const CreateProject = () => {
 					loadingText="Creating project"
 					form="form"
 				>
-					Create project
+					{submitButtonText}
 				</Button>
 			</Flex>
 		</Flex>
