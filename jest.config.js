9c294bf77c8319ad207fdf2bd5baafa21687bdd1
Makenna Smutz <makennasmutz@gmail.com>
2021-08-26 08:22:08 -0500

    update nx and packages

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,6 @@
+const { getJestProjects } = require('@nrwl/jest');
+
 module.exports = {
 	preset: 'jest-puppeteer',
-	projects: [
-		'<rootDir>/apps/webapp',
-		'<rootDir>/libs/chakra-theme',
-		'<rootDir>/apps/og-cards',
-		'<rootDir>/libs/meeshkan-types',
-		'<rootDir>/libs/downloadable-script',
-	],
+	projects: getJestProjects(),
 };
