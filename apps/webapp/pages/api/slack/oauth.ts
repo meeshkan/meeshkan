86adbea6a4df6aff32e9b6b060b4926b39ac4520
Makenna Smutz <makennasmutz@gmail.com>
2021-08-27 10:39:11 -0500

    add debugging

--- apps/webapp/pages/api/slack/oauth.ts
+++ apps/webapp/pages/api/slack/oauth.ts
@@ -10,69 +10,68 @@ export default async function slackOauthHook(
 ): Promise<void> {
 	const verificationCode = req?.query?.code as string;
 	const projectID = req?.query?.state as string;
-	// @ts-ignore
-	const auth0 = initAuth0(req);
-	const session = await auth0.getSession(req);
-	const idToken = session?.idToken;
+	const response = await fetch('/api/session');
+
 	console.log(
 		`Variables in Slack oauth`,
 		{ verificationCode },
 		{ projectID },
-		{ idToken }
+		{ req },
+		{ response: response.json() }
 	);
-	const client = eightBaseClient(idToken);
+	// const client = eightBaseClient(idToken);
 
-	if (!verificationCode || !projectID) {
-		console.error(
-			`The call has verificationCode: ${verificationCode}, and projectID: ${projectID} but needs both to be provided.`
-		);
-		res
-			.status(500)
-			.end(
-				`The call has verificationCode: ${verificationCode}, and projectID: ${projectID} but needs both to be provided.`
-			);
-	}
+	// if (!verificationCode || !projectID) {
+	// 	console.error(
+	// 		`The call has verificationCode: ${verificationCode}, and projectID: ${projectID} but needs both to be provided.`
+	// 	);
+	// 	res
+	// 		.status(500)
+	// 		.end(
+	// 			`The call has verificationCode: ${verificationCode}, and projectID: ${projectID} but needs both to be provided.`
+	// 		);
+	// }
 
-	const encodedAuthParams = encodeURIComponent(
-		`client_id=${process.env.NEXT_PUBLIC_SLACK_CLIENT_ID}&client_secret=${process.env.NEXT_PUBLIC_SLACK_CLIENT_SECRET}&code=${verificationCode}&redirect_uri=${window?.location?.origin}/api/slack/oauth`
-	);
-	console.log({ encodedAuthParams });
+	// const encodedAuthParams = encodeURIComponent(
+	// 	`client_id=${process.env.NEXT_PUBLIC_SLACK_CLIENT_ID}&client_secret=${process.env.NEXT_PUBLIC_SLACK_CLIENT_SECRET}&code=${verificationCode}&redirect_uri=${window?.location?.origin}/api/slack/oauth`
+	// );
+	// console.log({ encodedAuthParams });
 
-	try {
-		const oauthResponse = await fetch(
-			`https://slack.com/api/oauth.access?${encodedAuthParams}`,
-			{
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/x-www-form-urlencoded',
-				},
-			}
-		).then((res) => {
-			console.log(res.json());
-			return res.json();
-		});
+	// try {
+	// 	const oauthResponse = await fetch(
+	// 		`https://slack.com/api/oauth.access?${encodedAuthParams}`,
+	// 		{
+	// 			method: 'POST',
+	// 			headers: {
+	// 				'Content-Type': 'application/x-www-form-urlencoded',
+	// 			},
+	// 		}
+	// 	).then((res) => {
+	// 		console.log(res.json());
+	// 		return res.json();
+	// 	});
 
-		let redirectTo;
+	// 	let redirectTo;
 
-		if (oauthResponse.ok) {
-			const response = await client.request(CREATE_SLACK_CONFIGURATION, {
-				projectID,
-				accessToken: oauthResponse.access_token,
-				webhookChannel: oauthResponse.incoming_webhook.channel,
-				webhookChannelID: oauthResponse.incoming_webhook.channel_id,
-				webhookURL: oauthResponse.incoming_webhook.url,
-				webhookConfigurationURL:
-					oauthResponse.incoming_webhook.configuration_url,
-			});
-			console.log(response);
-			redirectTo = slugify(response.project.name);
-		}
+	// 	if (oauthResponse.ok) {
+	// 		const response = await client.request(CREATE_SLACK_CONFIGURATION, {
+	// 			projectID,
+	// 			accessToken: oauthResponse.access_token,
+	// 			webhookChannel: oauthResponse.incoming_webhook.channel,
+	// 			webhookChannelID: oauthResponse.incoming_webhook.channel_id,
+	// 			webhookURL: oauthResponse.incoming_webhook.url,
+	// 			webhookConfigurationURL:
+	// 				oauthResponse.incoming_webhook.configuration_url,
+	// 		});
+	// 		console.log(response);
+	// 		redirectTo = slugify(response.project.name);
+	// 	}
 
-		res.redirect(`/${redirectTo}`);
-	} catch (error) {
-		console.error(error);
-		res.status(error.status || 500).end(error.message);
-	}
+	// 	res.redirect(`/${redirectTo}`);
+	// } catch (error) {
+	// 	console.error(error);
+	// 	res.status(error.status || 500).end(error.message);
+	// }
 }
 
 // Next router - query: Object - The query string parsed to an object. It will be an empty object during prerendering if the page doesn't have data fetching requirements. Defaults to {}
